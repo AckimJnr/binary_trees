b378e90f408f44ec735c63e474887cfec6f9b4a8
AckimJnr <acklongwe@gmail.com>
2023-08-31 20:36:41 +0200

    Add update

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -18,9 +18,9 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	if (parent->left != NULL)
 	{
+		new_node->left = parent->left;
 		parent->left->parent = new_node;
 	}
-	new_node->left = parent->left;
 	parent->left = new_node;
 
 	return (new_node);
