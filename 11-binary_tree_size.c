fb70d5fcb9085f69c3f41b8e8790da58d6591bde
AckimJnr <acklongwe@gmail.com>
2023-09-01 23:25:52 +0200

    Update binary tree

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,21 +1,13 @@
 #include "binary_trees.h"
 /**
- * binary_tree_depth - Measures the depth of a node in a binary tree
- * @tree: Pointer to the node to measure the depth
- * Return: Depth of the node, or 0 if tree is NULL
+ * binary_tree_size - Measures the size of a binary tree.
+ * @tree: Pointer to the root node of the tree to measure the size.
+ * Return: Size of the tree, or 0 if tree is NULL.
  */
-size_t binary_tree_depth(const binary_tree_t *tree)
+size_t binary_tree_size(const binary_tree_t *tree)
 {
-	size_t depth = 0;
-
 	if (tree == NULL)
 		return (0);
 
-	while (tree->parent)
-	{
-		depth++;
-		tree = tree->parent;
-	}
-
-	return (depth);
+	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
 }
